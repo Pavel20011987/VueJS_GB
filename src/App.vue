66a2569b89a45fa24c326397ffd1664b675b629a
Pavel <45903339+Pavel20011987@users.noreply.github.com>
2023-11-02 19:59:03 +0300

    HW_6
    HW_6

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,33 @@
 <template>
-  <HeaderBlock />
-  <router-view></router-view>
-  <FooterBlock />
+  <HeaderComponent />
+  <!-- <HomepageComponent />
+  <ArticlesAndNewsComponent />
+  <BlogDetailsComponent /> -->
+  <ProjectsComponent />
+  <FooterComponent />
 </template>
 
 <script>
-import HeaderBlock from "./blocks/HeaderBlock";
-import FooterBlock from "./blocks/FooterBlock";
-
-
+import HomepageComponent from './components/HomepageComponent.vue';
+import ArticlesAndNewsComponent from './components/ArticlesAndNewsComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import BlogDetailsComponent from './components/BlogDetailsComponent.vue';
+import ProjectsComponent from './components/ProjectsComponent.vue';
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HeaderBlock,
-    FooterBlock,
-},
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    ArticlesAndNewsComponent,
+    BlogDetailsComponent,
+    ProjectsComponent
+}
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Jost:wght@400;500;600&display=swap');
-
-// $colorPrimary1: #cda274;
-// $colorPrimary2: #292f36;
-// $colorPrimary3: #f4f0ec;
-* {
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: "Jost", sans-serif;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  font-family: "DM Serif Display", serif;
-}
-
-a,
-button {
-  font-family: "Jost", sans-serif;
-  text-decoration: none;
-}
-
-a:hover,
-button:hover {
-  cursor: pointer;
-}
-
-input {
-  font-family: "Jost", sans-serif;
-}
-
-.center {
-  padding-left: calc(50% - 600px);
-  padding-right: calc(50% - 600px);
-}
-
-#app {
-  font-family: "Jost", sans-serif;
-}
-.swiper-pagination-bullet {
-  border: 1px solid #4d5053;
-  width: 25px;
-  height: 25px;
-  opacity: 1;
-  background-color: #fff;
-}
-
-.swiper-pagination-bullet-active {
-  background-color: #cda274;
-
-}
 
 </style>
